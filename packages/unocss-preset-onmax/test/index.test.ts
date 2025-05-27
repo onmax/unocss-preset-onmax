fdb2b3a69269e922b3649fab505d203bffc9b030
onmax <maximogarciamtnez@gmail.com>
2025-05-27 16:32:35 +0200

    feat: update matched.txt and output.css for improved styling and add focus-visible variants in tests

--- packages/unocss-preset-onmax/test/index.test.ts
+++ packages/unocss-preset-onmax/test/index.test.ts
@@ -11,7 +11,7 @@ const uno = await createGenerator({
 describe('basic setup', () => {
   it('presetOnmax', async () => {
     const presets = uno.config.presets
-    expect(presets).toHaveLength(9)
+    expect(presets).toHaveLength(10)
 
     const { css } = await uno.generate('text-4 var:test:cssvar f-text-xl bg-gradient-fn-from-blue', { preflights: false })
 
@@ -19,7 +19,7 @@ describe('basic setup', () => {
       "/* layer: shortcuts */
       .f-text-xl{--f-text-min:18;--f-text-max:22;font-size:clamp(calc(var(--f-text-unit, 1px) * var(--f-text-min, 16)), calc(var(--f-text-unit, 1px) * var(--f-text-min, 16) + (var(--f-text-max, 16) - var(--f-text-min, 16)) * (var(--f-text-container, 100vw) - (var(--f-text-unit, 1px) * var(--f-text-min-container, 320))) / (var(--f-text-max-container, 1920) - var(--f-text-min-container, 320))), calc(var(--f-text-unit, 1px) * var(--f-text-max, 16)));}
       /* layer: default */
-      .text-4{font-size:1rem;}
+      .text-4{font-size:0.25rem;}
       .var\\:test\\:cssvar{--test:cssvar;}
       .bg-gradient-fn-from-blue{--un-gradient-fn-from:oklch(0.707 0.165 254.624);}"
     `)
